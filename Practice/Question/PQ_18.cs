da989bafb18fb2d2f7d4a156bd5fbff09a2b8188
GonoBae <qldrhqorhsh@naver.com>
2022-06-14 22:45:03 +0900

    220614

---
+++ Practice/Question/PQ_18.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Question
+{
+    class PQ_18
+    {
+        public int solution(int[] a, int[] b)
+        {
+            int answer = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                answer += a[i] * b[i];
+            }
+            return answer;
+        }
+
+        public static void Main1()
+        {
+            PQ_18 pq = new PQ_18();
+            int[] a = { 1, 2, 3, 4 };
+            int[] b = { -3, -1, 0, 2 };
+            pq.solution(a, b);
+        }
+    }
+}
