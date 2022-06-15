69f24e57d80315d07bc0b74d675bd5f4b0a0b699
GonoBae <qldrhqorhsh@naver.com>
2022-06-15 22:53:03 +0900

    220615

--- Practice/Question/PQ_19.cs
+++ Practice/Question/PQ_19.cs
@@ -8,6 +8,16 @@ namespace Practice.Question
 {
     class PQ_19
     {
-
+        int Solution(string _str)
+        {
+            int count = 0;
+            count = _str.Split(' ').Length;
+            return count;
+        }
+        public static void Main()
+        {
+            PQ_19 p = new PQ_19();
+            p.Solution("std std std");
+        }
     }
 }
