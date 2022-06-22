f0f5dc5ce38969b4baf88b4383b7c4a3bfa1b2be
GonoBae <qldrhqorhsh@naver.com>
2022-06-23 00:20:15 +0900

    220623

---
+++ Practice/Question/PQ_21.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 프린터 => Queue 를 이용
+
+namespace Practice.Question.PQ_21
+{
+    public class Solution
+    {
+        public int solution(int[] priorities, int location)
+        {
+            int answer = 0;
+            Queue<KeyValuePair<int, int>> q = new Queue<KeyValuePair<int, int>>();
+            
+            for(int i = 0; i < priorities.Length; i++)
+            {
+                q.Enqueue(new KeyValuePair<int, int>(i, priorities[i]));
+            }
+            while (true)
+            {
+                int max = q.Max(x => x.Value);
+                KeyValuePair<int, int> first = q.Dequeue();
+                if (max == first.Value)
+                {
+                    if (first.Key == location) return answer + 1;
+                    else
+                    {
+                        answer++;
+                        continue;
+                    }
+                }
+                q.Enqueue(first);
+            }
+        }
+
+        public static void Main1()
+        {
+            Solution solution = new Solution();
+            int[] priorites = { 1, 2, 3, 2, 3 };
+            int location = 3;
+            int answer = solution.solution(priorites, location);
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Practice/Question/PQ_22.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 3진법 뒤집기
+
+namespace Practice.Question.Q_22
+{
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            List<int> list = new List<int>();
+            while(n > 0)
+            {
+                int num = n % 3;
+                list.Add(num);
+                n /= 3;
+            }
+            list.Reverse();
+            for(int i = 0; i < list.Count; i++)
+            {
+                answer += (int)(list[i] * Math.Pow(3, i));
+            }
+            return answer;
+        }
+        public static void Main1()
+        {
+            Solution solution = new Solution();
+            int answer = solution.solution(125);
+            Console.WriteLine(answer);
+        }
+    }
+}
