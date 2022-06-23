0a1ad550f9aa6d19ba7ee703f6dda1ffae011160
GonoBae <qldrhqorhsh@naver.com>
2022-06-24 02:05:27 +0900

    220624

---
+++ Practice/Question/PQ_23.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Question.PQ_23
+{
+    public class Solution
+    {
+        public int solution(string name)
+        {
+            int answer = 0;
+            int n = name.Length;
+            int leftOrRight = name.Length - 1;
+            for(int i = 0; i < n; i++)
+            {
+                int next = i + 1;
+                char target = name[i];
+                if (target <= 'N') answer += target - 'A';
+                else answer += 'Z' - target + 1;
+
+                while (next < n && name[next] == 'A') next++;
+                leftOrRight = Math.Min(leftOrRight, i + n - next + Math.Min(i, n - next));
+            }
+            answer += leftOrRight;
+            return answer;
+        }
+
+        public static void Main1()
+        {
+            Solution solution = new Solution();
+            string str = "BBAAABAAAAAAAAAAAABA";
+            int num = solution.solution(str);
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Practice/Question/PQ_24.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Question.PQ_24
+{
+    public class Solution
+    {
+        public int solution(int[,] sizes)
+        {
+            int answer = 0;
+            int xMax = 0, yMax = 0;
+            
+            for(int i = 0; i < sizes.GetLength(0); i++)
+            {
+                if (sizes[i, 0] < sizes[i, 1])
+                {
+                    int temp = sizes[i, 0];
+                    sizes[i, 0] = sizes[i, 1];
+                    sizes[i, 1] = temp;
+                }    
+
+                if(xMax < sizes[i, 0])
+                {
+                    xMax = sizes[i, 0];
+                }
+                
+                if(yMax < sizes[i, 1])
+                {
+                    yMax = sizes[i, 1];
+                }
+            }
+
+            answer = xMax * yMax;
+            return answer;
+        }
+
+        public static void Main()
+        {
+            Solution solution = new Solution();
+            int[,] sizes = { { 60, 50 }, { 30, 70 }, { 60, 30 }, { 80, 40 } };
+            int[,] sizes1 = { { 10, 7 }, { 12, 3 }, { 8, 15 }, { 14, 7 }, { 5, 15 } };
+            int[,] sizes2 = { { 14, 4 }, { 19, 6 }, { 6, 16 }, { 18, 7 }, { 7, 11 } };
+            int n = solution.solution(sizes2);
+            Console.WriteLine(n);
+        }
+    }
+}
