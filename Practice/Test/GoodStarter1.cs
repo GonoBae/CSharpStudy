652b65b01eb3908b1c3f87315f32e67e33a165c4
GonoBae <qldrhqorhsh@naver.com>
2022-07-13 23:55:47 +0900

    220713

---
+++ Practice/Test/GoodStarter1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Test.GoodStarter1
+{
+    public class Solution
+    {
+        public string solution(int num)
+        {
+            string answer = "";
+            if (num % 2 == 0) answer = "Even";
+            else answer = "Odd";
+            return answer;
+        }
+    }
+}
---
+++ Practice/Test/GoodStarter2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Test.GoodStarter2
+{
+    public class Solution
+    {
+        public bool solution(int x)
+        {
+            bool answer = true;
+            int a = 0, b = x, sum = 0;
+            while (b != 0)
+            {
+                a = b % 10;
+                b /= 10;
+                sum += a;
+            }
+            if (x % sum == 0) answer = true;
+            else answer = false;
+            return answer;
+        }
+    }
+}
